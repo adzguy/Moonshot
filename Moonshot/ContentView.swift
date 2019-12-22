b1b277c84981c3615f3df0b2c3e801a85896219d
adzguy <dnurmeto@neiu.edu>
2019-12-22 02:05:52 -0600

    NavigationLink

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,29 +9,16 @@
 import SwiftUI
 
 
-struct CustomText: View {
-    
-    var text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical) {
-            VStack (spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
+        NavigationView {
+            List(0..<100) { row in
+                NavigationLink(destination: Text("Detail \(row)")) {
+                    Text("Row \(row)")
                 }
+                
             }
-            .frame(maxWidth: .infinity)
+        .navigationBarTitle("SwiftUI")
         }
     }
 }
